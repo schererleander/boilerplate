6b2f8df6e0a8153d28016f4e25272c1068536e94
schererleander <leander@schererleander.de>
2025-07-02 22:20:08 +0200

    feat: add User model

---
+++ src/model/User.ts
@@ -0,0 +1,18 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profileImage: {
+    url: { type: String },
+    key: { type: String },
+    uploadedAt: { type: Date } 
+  }
+}, {
+  timestamps: true
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User; 
