bade0f61081ca84270a2e9223393e62f80b38af9
schererleander <leander@schererleander.de>
2025-12-26 18:08:56 +0100

    refactor(ui): simplify dialog component by removing close button

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -2,43 +2,19 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-interface MongooseCache {
-  conn: typeof mongoose | null;
-  promise: Promise<typeof mongoose> | null;
-}
-
-declare global {
-  var mongoose: MongooseCache | undefined;
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 async function dbConnect() {
   if (!MONGODB_URI) {
-    throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+    throw new Error(
+      "Please define the MONGODB_URI environment variable inside .env.local"
+    );
   }
 
-  if (cached!.conn) return cached!.conn;
+  // If the connection is already established, return
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
 
-  if (!cached!.promise) {
-    const opts = { bufferCommands: false };
-    cached!.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  
-  try {
-    cached!.conn = await cached!.promise;
-  } catch (e) {
-    cached!.promise = null;
-    throw e;
-  }
-  
-  return cached!.conn;
+  return mongoose.connect(MONGODB_URI);
 }
 
 export default dbConnect;
